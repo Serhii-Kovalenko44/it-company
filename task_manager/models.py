9f7d70765f9eec9d911074fefd7d2cca1f98e468
Serhii Kovalenko <27kovalenko@gmail.com>
2023-03-15 17:11:05 +0200

    Created the Position model in models.py

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,7 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class TaskType(models.Model):
+    name = models.CharField(max_length=63)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Position(models.Model):
     name = models.CharField(max_length=63)
 
     def __str__(self) -> str:
