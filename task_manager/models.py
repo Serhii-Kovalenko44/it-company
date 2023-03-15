b7f799b4bcbe173142e11adff38b1453c56cd3c1
Serhii Kovalenko <27kovalenko@gmail.com>
2023-03-15 17:57:19 +0200

    Created the Task model in models.py

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -23,5 +24,24 @@ class Worker(AbstractUser):
         verbose_name = "worker"
         verbose_name_plural = "workers"
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Task(models.Model):
+    PRIORITY = (
+        (0, "Low"),
+        (1, "Medium"),
+        (2, "High"),
+        (3, "Urgent"),
+    )
+    name = models.CharField(max_length=63)
+    description = models.TextField()
+    deadline = models.DateField
+    is_completed = models.BooleanField()
+    priority = models.IntegerField(max_length=1, choices=PRIORITY, default=0)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="task")
+
+    def __str__(self) -> str:
+        return self.name
