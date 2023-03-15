dfb4f8d621293f9d489080031f5a0f07e8dcacff
Serhii Kovalenko <27kovalenko@gmail.com>
2023-03-15 17:23:35 +0200

    Created the Worker model in models.py

--- task_manager/models.py
+++ task_manager/models.py
@@ -14,3 +14,14 @@ class Position(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
