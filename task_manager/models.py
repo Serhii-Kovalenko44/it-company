ebe6be6f3932c6d529e03dd3edd8cea9e702dfda
Serhii Kovalenko <27kovalenko@gmail.com>
2023-03-15 16:56:02 +0200

    Created the TaskType model in models.py

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=63)
+
+    def __str__(self) -> str:
+        return self.name
