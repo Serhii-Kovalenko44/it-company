939c8ebbf017eab719ad0de6505b2279412f1424
Serhii Kovalenko <27kovalenko@gmail.com>
2023-03-17 23:04:51 +0200

    Added a view index

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from task_manager.models import TaskType, Position, Worker, Task
+
+
+@login_required
+def index(request) -> render:
+    num_task_type = TaskType.objects.count()
+    num_position = Position.objects.count()
+    num_worker = Worker.objects.count()
+    num_task = Task.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_task_type": num_task_type,
+        "num_position": num_position,
+        "num_worker": num_worker,
+        "num_task": num_task,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "task_manager/index.html", context=context)
