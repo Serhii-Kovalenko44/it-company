22afae716d6028cc387c9b73e40f5fa242fc9b5c
Serhii Kovalenko <27kovalenko@gmail.com>
2023-03-27 19:35:27 +0300

    Added paths to the position/, task_type/, worker/, task/

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from task_manager.views import index
+from task_manager.views import index, PositionListView, TaskTypeListView, WorkerListView, TaskListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("position/", PositionListView.as_view(), name="position-list"),
+    path("task-type/", TaskTypeListView.as_view(), name="task-type-list"),
+    path("worker/", WorkerListView.as_view(), name="worker-list"),
+    path("task/", TaskListView.as_view(), name="task-list"),
+
 ]
 
 app_name = "task_manager"
