bf0dda196fdbd7c702e9cc59754b53f58f6b4a91
Serhii Kovalenko <27kovalenko@gmail.com>
2023-03-28 17:21:44 +0300

    Added path on the PositionDetailView

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 
-from task_manager.views import index, PositionListView, TaskTypeListView, WorkerListView, TaskListView
+from task_manager.views import (
+    index,
+    PositionListView,
+    PositionDetailView,
+    TaskTypeListView,
+    WorkerListView,
+    TaskListView,
+)
 
 urlpatterns = [
     path("", index, name="index"),
     path("position/", PositionListView.as_view(), name="position-list"),
+    path("position/<int:pk>/", PositionDetailView.as_view(), name="position-detail"),
     path("task-type/", TaskTypeListView.as_view(), name="task-type-list"),
     path("worker/", WorkerListView.as_view(), name="worker-list"),
     path("task/", TaskListView.as_view(), name="task-list"),
