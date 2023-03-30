acd8f08cbc6e9375aa389999037df08133f3818c
Serhii Kovalenko <27kovalenko@gmail.com>
2023-03-30 17:09:57 +0300

    Updated path to the TaskDeleteView

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -7,7 +7,10 @@ from task_manager.views import (
     TaskTypeListView,
     WorkerListView,
     TaskListView,
-    TaskDetailView, TaskCreateView, TaskUpdateView,
+    TaskDetailView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
 )
 
 urlpatterns = [
@@ -20,6 +23,7 @@ urlpatterns = [
     path("task/create/", TaskCreateView.as_view(), name="task-create"),
     path("task/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
     path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 
 ]
 
