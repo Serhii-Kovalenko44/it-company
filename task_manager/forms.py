b985a2fb9ca5b51a72e811148208dad9b657a52b
Serhii Kovalenko <27kovalenko@gmail.com>
2023-04-03 16:49:26 +0300

    Added TaskSearchForm in the forms.py

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 
-from task_manager.models import Task
+from task_manager.models import Task, Worker
 
 
 class TaskForm(forms.ModelForm):
@@ -13,3 +14,33 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = "__all__"
+
+
+class WorkerCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Worker
+        fields = UserCreationForm.Meta.fields + (
+            "position",
+            "first_name",
+            "last_name",
+        )
+
+
+class WorkerUpdateDataForm(forms.ModelForm):
+    class Meta:
+        model = Worker
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "position",
+        ]
+
+
+class TaskSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=63,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by name"})
+    )
